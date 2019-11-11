e7c706e1aa6ba851281584065e8b002cb78c1217
Alexandre Locci Martins <alexandre.locci@gmail.com>
2019-11-11 11:24:50 -0300

    teste

---
+++ database/stages/teste.lua
@@ -0,0 +1,15 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: alexandre
+-- Date: 11/11/2019
+-- Time: 10:21
+-- To change this template use File | Settings | File Templates.
+--
+
+return {
+    title = 'Teste',
+    waves = {
+        { blue_slime = 1}
+    }
+}
+
---
+++ database/units/blue_smile.lua
@@ -0,0 +1,13 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: alexandre
+-- Date: 11/11/2019
+-- Time: 10:23
+-- To change this template use File | Settings | File Templates.
+--
+return {
+    name = "Blue Slime",
+    max_hp = 4,
+    appearance = 'blue_slime'
+}
+
