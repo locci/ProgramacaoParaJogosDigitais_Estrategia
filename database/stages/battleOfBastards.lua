22f94dc090051698924833213bce7d3d76889403
Alexandre Locci Martins <alexandre.locci@gmail.com>
2019-11-22 11:09:55 -0300

    criando objetos

--- database/stages/battleOfBastards.lua
+++ database/stages/battleOfBastards.lua
@@ -3,5 +3,10 @@ return {
   title = 'Batle of Bastards',
   waves = {
     { archer = 1 },
+  },
+  landscape ={
+    {{type = 'rock', num = 20}, {type = 'tree', num = 13}},
+    {'tree', 4 },
+    {'tree', 5 },
   }
 }
--- database/stages/clashRoyale.lua
+++ database/stages/clashRoyale.lua
@@ -3,5 +3,10 @@ return {
   title = 'Clash Royale',
   waves = {
     { knight = 1 },
+  },
+  landscape ={
+    {{type = 'rock', num = 5}, {type = 'tree', num = 13}},
+    {'tree', 4 },
+    {'tree', 5 },
   }
 }
