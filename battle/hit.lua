24fd44740b8f79e4e589a937a09719bc6318c90b
APF2000 <arthurpfonseca@gmail.com>
2019-12-06 02:15:29 -0300

    Deu bom, nao sabemos pq

--- battle/hit.lua
+++ battle/hit.lua
@@ -18,24 +18,35 @@ end
 function updateDamage(dt)
   for _, unit1 in pairs(units) do
     for _, unit2 in pairs(units) do
-      if unit1:isHero() and unit2:isMonster() or
-      unit2:isHero() and unit1:isMonster() then
-        local dist = (unit1:getPos() - unit2:getPos()):length()
+      local dist = (unit1:getPos() - unit2:getPos()):length()
+      if unit1:isHero() and unit2:isMonster() then
         if dist <= unit1:getFieldRadius() then
           --porradaria franca
-          if unit2:get_hp() > 0 then
+          if unit2:get_hp() > 0 and unit1:get_hp() > 0 then
             unit2:changeHp(unit1:getHitDamage())
+            print("unit1=", unit1:get_name(), unit1:get_hp())
+            print("\nunit2=", unit2:get_name(), unit2:get_hp())
           end
-          print("unit1=", unit1:get_name(), unit1:get_hp())
-          print("\nunit2=", unit2:get_name(), unit2:get_hp())
+        end
+      elseif unit2:isHero() and unit1:isMonster() then
+        if dist <= unit1:getFieldRadius() then
+          --porradaria franca
+          if unit2:get_hp() > 0 and unit1:get_hp() > 0 then
+            unit2:changeHp(unit1:getHitDamage())
+            print("unit1=", unit1:get_name(), unit1:get_hp())
+            print("\nunit2=", unit2:get_name(), unit2:get_hp())
+          end
         end
       end
       if unit1:isHealer() and unit2:isHero() then
-        unit2:changeHp(unit1:getHitDamage())
-        print("\nunit2=", unit2:get_name(), unit2:get_hp())
+        if dist <= unit1:getFieldRadius() then
+          unit2:changeHp(unit1:getHitDamage())
+          print("\nunit2=", unit2:get_name(), unit2:get_hp())
+        end
       elseif unit2:isHealer() and unit1:isHero() then
-        --print("\nunit1=", unit1:get_name(), unit1:get_hp())
-        unit1:changeHp(unit2:getHitDamage())
+        if dist <= unit2:getFieldRadius() then
+          unit1:changeHp(unit2:getHitDamage())
+        end
       end
     end
   end
